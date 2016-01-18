260e64ab7898b594eb020def5a4cbc597187c01b
plus <pccycq@gmail.com>
2016-01-18 14:29:40 +0200

    consistent console and menu

--- main.py
+++ main.py
@@ -1,20 +1,46 @@
 import sys
 from PyQt5.QtWidgets import (QPushButton, QMainWindow, QApplication, QWidget,
                              QDialog, QStackedWidget, QLineEdit, QHBoxLayout,
-                             QVBoxLayout)
+                             QVBoxLayout, QLabel, qApp, QAction)
+from PyQt5.QtCore import Qt
 
 
 class Main(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
+        global console
+
+        console = QLineEdit(self)
+        console.setStyleSheet("background-color: #BEBEBE; color: #333")
+        console.textChanged.connect(handleEntry)
+        console.returnPressed.connect(handleEntryReturn)
 
         self.stacked = QStackedWidget()
-        self.setCentralWidget(self.stacked)
-        self.switchMainView()
+        self.mainLayout = QVBoxLayout()
+        self.mainLayout.setContentsMargins(0, 0, 0, 0)
+        self.mainLayout.addWidget(self.stacked)
+        self.mainLayout.addWidget(console)
+        mainWidget = QWidget()
+        mainWidget.setLayout(self.mainLayout)
+        self.switchBookView() #
 
-        self.setGeometry(100, 100, 800, 600)
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        exitAction = QAction('&Exit', self)
+        exitAction.triggered.connect(qApp.quit)
+        fileMenu.addAction(exitAction)
+        switchMenu = menubar.addMenu('&Switch')
+        switchMainAction = QAction('&Main Menu', self)
+        switchMainAction.triggered.connect(self.switchMainView)
+        switchMenu.addAction(switchMainAction)
+        switchBookAction = QAction('&Book1', self)
+        switchBookAction.triggered.connect(self.switchBookView)
+        switchMenu.addAction(switchBookAction)
+        # these menus are temporary
+
+        self.setCentralWidget(mainWidget)
+        self.setGeometry(100, 300, 800, 600)
         self.setWindowTitle('retype')
-        self.show()
 
     def switchMainView(self):
         mainView = MainView(self)
@@ -32,13 +58,8 @@ class MainView(QWidget):
         super().__init__(parent)
 
         self.layout = QVBoxLayout()
-        book1 = QPushButton('book1', self)
-        book1.clicked.connect(self.parent().switchBookView)
-        console = QLineEdit(self)
-        self.layout.addWidget(book1)
-        self.layout.addStretch(0)
-        self.layout.addWidget(console)
-        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.label = QLabel('Main View', self)
+        self.layout.addWidget(self.label)
         self.setLayout(self.layout)
 
 
@@ -46,11 +67,24 @@ class BookView(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        backBtn = QPushButton('back to main', self)
-        backBtn.clicked.connect(self.parent().switchMainView)
+        self.layout = QVBoxLayout()
+        self.label = QLabel('Book View', self)
+        self.layout.addWidget(self.label)
+        self.setLayout(self.layout)
 
+
+def handleEntry():
+    print(console.text())
+
+
+def handleEntryReturn():
+    entry = console.text()
+
+    if entry == 'book1':
+        retype.switchBookView()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     retype = Main()
+    retype.show()
     sys.exit(app.exec_())
