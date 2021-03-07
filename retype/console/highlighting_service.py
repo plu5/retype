plu5/retype

commit 96acca81a4f6f4fff312818bb21e905d8ffb7181

More modeline changes (path tooltip, singular update function, ..)
and fixed a 'bug' i introduced when refactoring highlighting service where it
will crash if you try to switch to book view without a book loaded since
highlighting variables have not been initialised.

retype/console/highlighting_service.py
@@ -27,6 +27,9 @@ class HighlightingService(object):
 
         if not v.isVisible():
             return
+        # In case there is no book loaded / variables not been initialised
+        if not getattr(v, 'persistent_pos', False):
+            return
 
         # Remove highlighting if things were deleted
         if len(text) + v.persistent_pos < v.cursor_pos:
@@ -75,7 +78,6 @@ class HighlightingService(object):
 
         self.updateHighlighting()
         self._console.clear()
-        # v.display.setTextCursor(v.cursor)
         v.display.centreAroundCursor()
 
     def updateHighlighting(self):

retype/controllers/library.py
@@ -45,7 +45,7 @@ class LibraryController(object):
 
 class BookWrapper(object):
     def __init__(self, path, idn):
-        self._path = path
+        self.path = path
         self._book = epub.read_epub(path)
         self.idn = idn
         self.title = self._book.title

retype/ui/book_view.py
@@ -113,16 +113,18 @@ class BookView(QWidget):
 
     def _initModeline(self):
         self.modeline = Modeline(self)
-        self.modeline.setTitle('No book loaded')
+        self.modeline.update_(title="No book loaded")
 
     def updateModeline(self):
-        self.modeline.setTitle(self.book.title)
-        self.modeline.setCursorPos(self.cursor_pos)
-        self.modeline.setLinePos(self.line_pos)
-        self.modeline.setChapPos(self.chapter_pos)
-        self.modeline.setViewedChapPos(self.viewed_chapter_pos)
-        self.modeline.setChapTotal(len(self.book.chapters) - 1)
-        self.modeline.repaint()
+        self.modeline.update_(
+            title=self.book.title,
+            path=self.book.path,
+            cursor_pos=self.cursor_pos,
+            line_pos=self.line_pos,
+            chap_pos=self.chapter_pos,
+            viewed_chap_pos=self.viewed_chapter_pos,
+            chap_total=len(self.book.chapters) - 1
+        )
 
     def _initChapter(self):
         if not self.chapter_pos:

retype/ui/modeline.py
@@ -48,11 +48,11 @@ class Spacer(QWidget):
         self.p = parent
         self.width = width
         if self.p and hasattr(self.p, 'repainted'):
-            self.p.repainted.connect(self.update)
+            self.p.repainted.connect(self.update_)
         else:
             self.setFixedWidth(self.width)
 
-    def update(self):
+    def update_(self):
         if not self.p:
             return
         pwidth = self.p.size().width()
@@ -100,10 +100,10 @@ class Modeline(QWidget):
         self.setStyleSheet("*[css_~='hover']:hover{background: white}")
 
         # Dynamic
+        self.title = QLabel("")
+        self.path = QLabel("")
         self.cursor_pos = QLabel(str(0))
         self.line_pos = QLabel(str(0))
-        self.title = QLabel("")
-        self.path = ""  # TODO
         self.chap_pos = QLabel(str(0))
         self.viewed_chap_pos = QLabel(str(0))
         # For chapTotal I need two: one for the group with cursor chapter
@@ -129,7 +129,7 @@ class Modeline(QWidget):
         l_group = makeGroup(self.cursor_pos, self.pos_sep, self.line_pos,
                             tooltip="Line and character position of cursor")
 
-        t_group = makeGroup(self.title, tooltip="path (TODO)")
+        self.t_group = makeGroup(self.title, tooltip="?")
         self.title.setStyleSheet("font-weight:bold")
 
         c_group = makeGroup(self.chap_pre, self.chap_pos,
@@ -145,35 +145,35 @@ class Modeline(QWidget):
         self.layout_.setContentsMargins(0, self.padding, 0, 0)
         self.layout_.setSpacing(0)
 
-        self.separators = [Separator(self.colour1, self.padding),
-                           Separator(self.colour2, self.padding),
-                           Separator(self.colour3, self.padding, False),
-                           Separator(self.colour2, self.padding, False)]
-        self.elements = [l_group, self.separators[0], t_group,
-                         self.separators[1], c_group, Spacer(self.padding),
-                         v_group, Spacer(45, self), self.separators[2],
-                         self.separators[3]]
+        self.separators = [
+            Separator(self.colour1, self.padding),
+            Separator(self.colour2, self.padding),
+            Separator(self.colour3, self.padding, False),
+            Separator(self.colour2, self.padding, False)
+        ]
+        self.elements = [
+            l_group, self.separators[0],
+            self.t_group, self.separators[1],
+            c_group, Spacer(self.padding), v_group, Spacer(45, self),
+            self.separators[2], self.separators[3]
+        ]
         for element in self.elements:
             self.layout_.addWidget(element)
 
-    def setCursorPos(self, value):
-        self.cursor_pos.setText(str(value))
+    def update_(self, title=None, cursor_pos=None, line_pos=None,
+                chap_pos=None, viewed_chap_pos=None, chap_total=None,
+                path=None):
+        for p, v in vars().items():
+            if v and p != "self":
+                self.__dict__[p].setText(str(v))
 
-    def setTitle(self, title):
-        self.title.setText(title)
+        if chap_total:
+            self.chap_total_dupe.setText(str(chap_total))
 
-    def setLinePos(self, value):
-        self.line_pos.setText(str(value))
+        if path:
+            self.t_group.setToolTip(path)
 
-    def setChapPos(self, value):
-        self.chap_pos.setText(str(value))
-
-    def setViewedChapPos(self, value):
-        self.viewed_chap_pos.setText(str(value))
-
-    def setChapTotal(self, value):
-        self.chap_total.setText(str(value))
-        self.chap_total_dupe.setText(str(value))
+        self.repaint()
 
     def paintEvent(self, e):
         width = self.size().width()

commit 39070181ab02c0113ee113a5becb9a7bf6fd6b2a

HighlightingService: Fix bug introduced in 96acca8 and exceptions

retype/console/highlighting_service.py
@@ -28,7 +28,7 @@ class HighlightingService(object):
         if not v.isVisible():
             return
         # In case there is no book loaded / variables not been initialised
-        if not getattr(v, 'persistent_pos', False):
+        if getattr(v, 'persistent_pos', False) is False:
             return
 
         # Remove highlighting if things were deleted
@@ -47,7 +47,7 @@ class HighlightingService(object):
             while v.current_line.isspace() or v.current_line == '':
                 try:
                     self.advanceLine()
-                except e as e:
+                except Exception as e:
                     logger.error('empty lines loop exit', e)
                     return
 
@@ -70,7 +70,7 @@ class HighlightingService(object):
         # Set the line that needs to be typed next
         try:
             v.setLine(v.line_pos)
-        except e as e:
+        except Exception as e:
             logger.error('can’t advance line \
             {}/{}'.format(v.line_pos, len(v.to_be_typed_list)),
                          exc_info=True)