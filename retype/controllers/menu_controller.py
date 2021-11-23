014b41af56ba42254896b91f6c3b23e67470b9f0
plus <ctre@users.sourceforge.net>
2021-11-23 09:21:10 +0200

    Menu additions

--- retype/controllers/menu_controller.py
+++ retype/controllers/menu_controller.py
@@ -1,12 +1,12 @@
 from PyQt5.Qt import QAction, QObject
 
-from retype.constants import issue_tracker
+from retype.constants import RETYPE_ISSUE_TRACKER_URL, RETYPE_DOCUMENTATION_URL
 
 
 class MenuController(QObject):
     def __init__(self, main_controller, menu):
         super().__init__()
-        self._main_controller = main_controller
+        self.controller = main_controller
         self._menu = menu
         self._initMenuBar()
 
@@ -29,23 +29,26 @@ class MenuController(QObject):
 
     def _fileMenu(self):
         fileMenu = self._menu.addMenu('&File')
-        self._addAction(fileMenu, '&Quit', self._main_controller.quit,
+        self._addAction(fileMenu, '&Quit', self.controller.quit,
                         ['Alt+F4'])
 
     def _viewMenu(self):
         viewMenu = self._menu.addMenu('&View')
         self._addAction(viewMenu, '&Shelf View',
-                        lambda: self._main_controller.setViewByEnum(1))
+                        lambda: self.controller.setViewByEnum(1), ['Ctrl+1'])
         self._addAction(viewMenu, '&Book View',
-                        lambda: self._main_controller.setViewByEnum(2))
+                        lambda: self.controller.setViewByEnum(2), ['Ctrl+2'])
 
     def _optionsMenu(self):
         optionsMenu = self._menu.addMenu('&Options')
         self._addAction(optionsMenu, '&Customise retype',
-                        self._main_controller.showCustomisationDialog)
+                        self.controller.showCustomisationDialog, ['Ctrl+O'])
 
     def _helpMenu(self):
         helpMenu = self._menu.addMenu('&Help')
+        self._addAction(helpMenu, "&About", self.controller.showAboutDialog)
+        self._addAction(helpMenu, "&Documentation (opens in browser)", lambda:
+                        self.controller.openUrl(RETYPE_DOCUMENTATION_URL))
         self._addAction(helpMenu,
-                        '&Report issue (opens in browser)',
-                        lambda: self._main_controller.openUrl(issue_tracker))
+                        '&Report issue (opens in browser)', lambda:
+                        self.controller.openUrl(RETYPE_ISSUE_TRACKER_URL))
