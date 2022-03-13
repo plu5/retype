a47245517168aa06d7314d087d058b95786c1630
plus <ctre@users.sourceforge.net>
2022-03-13 17:08:06 +0200

    Add loglevel cli arg

--- retype/app.py
+++ retype/app.py
@@ -1,16 +1,34 @@
 import sys
 import logging
+import argparse
 from qt import QApplication
 
 from retype.controllers import MainController
 
 
+_logging_levels = logging._levelToName  # FIXME: bad
+_logging_levels_names = [n for n in _logging_levels.values()]
+
+
 def run():
     app = QApplication(sys.argv)
-    _configLog(0)
+
+    args = _parseArgs()
+    _configLog(args.loglevel[0])
+
     controller = MainController()
     controller.show()
     sys.exit(app.exec())
+
+
+def _parseArgs():
+    parser = argparse.ArgumentParser(description='retype')
+    parser.add_argument(
+        '-l', '--loglevel', type=str, nargs=1, metavar='LOGLEVEL',
+        default=['WARNING'], choices=_logging_levels_names,
+        help=f'Set logging output level to one of:\
+ {", ".join(_logging_levels_names)}')
+    return parser.parse_args()
 
 
 def _configLog(level):
