ce557ae0e295de2e902143f1ba3818ddc4347686
plu5 <plu5@users.noreply.github.com>
2022-06-01 15:24:09 +0300

    Create ubuntu-setup-python.sh

---
+++ .github/workflows/ubuntu-setup-python.sh
@@ -0,0 +1,14 @@
+VERSION="3.7.4"
+
+apt update && apt upgrade -y && apt clean && apt update
+apt-get install -y wget build-essential checkinstall  libreadline-gplv2-dev  libncursesw5-dev  libssl-dev  libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+
+cd /usr/src
+wget https://www.python.org/ftp/python/${VERSION}/Python-${VERSION}.tgz
+tar xzf Python-${VERSION}.tgz
+cd Python-${VERSION}
+
+./configure --enable-optimizations --enable-shared
+make altinstall
+
+ldconfig /usr/local/lib
