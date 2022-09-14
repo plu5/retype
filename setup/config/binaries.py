e513b5dfc1fccc15f56084e512b38f0440cdc838
plus <ctre@users.sourceforge.net>
2022-09-14 16:00:27 +0300

    Setup: Add core-libraryloader to files to exclude in win builds

--- setup/config/binaries.py
+++ setup/config/binaries.py
@@ -8,7 +8,7 @@ if iswindows:
         'Qt5DBus.dll', 'opengl32sw.dll', 'Qt5Network.dll', 'Qt5Qml.dll',
         'Qt5QmlModels.dll', 'Qt5Quick.dll', 'Qt5Svg.dll', 'Qt5WebSockets.dll',
         '_bz2', '_hashlib', '_socket', '_ssl', 'libcrypto-1_1.dll',
-        'libssl-1_1.dll', 'objectify', 'sax',
+        'libssl-1_1.dll', 'objectify', 'sax', 'core-libraryloader',
     }
 elif islinux:
     to_exclude = {
