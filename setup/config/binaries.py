fa6abf8758e7bd6e010a93e7a6f5156d763f72f0
plu5 <plu5@users.noreply.github.com>
2024-04-25 10:47:13 +0300

    Remove exclusions

--- setup/config/binaries.py
+++ setup/config/binaries.py
@@ -10,34 +10,9 @@ to_add = []
 
 if iswindows:
     to_exclude = {
-        'Qt5DBus.dll', 'opengl32sw.dll', 'Qt5Network.dll', 'Qt5Qml.dll',
-        'Qt5QmlModels.dll', 'Qt5Quick.dll', 'Qt5Svg.dll', 'Qt5WebSockets.dll',
-        '_bz2', '_socket', '_ssl', 'libcrypto-1_1.dll', 'libssl-1_1.dll',
-        'objectify', 'sax', 'core-libraryloader',
     }
 elif islinux:
     to_exclude = {
-        'libgtk-3', 'libQt5Quick', 'libQt5Qml', 'libcrypto', 'libQt5Network',
-        'libQt5WaylandClient', '_decimal', 'libgio-2.0', 'libstdc++', 'libX11',
-        'unicodedata', 'pyexpat', 'libglib-2.0', 'libcairo', 'libgcrypt',
-        'libepoxy', 'libgdk', 'libkrb5', 'libQt5QmlModels', 'objectify', 'sax',
-        'libfreetype', 'libpixman', 'libmirclient', '_pickle',
-        '_datetime', '_ctypes', 'libsystemd', 'libharfbuzz',
-        'libmirprotobuf', 'libssl', 'libpcre', '_ssl', '_json', 'libgobject',
-        '_codecs', 'libprotobuf', 'libpango', 'libdbus', 'libgssapi',
-        'libmount', 'libQt5WebSockets', 'libfontconfig', 'libblkid',
-        'libxkbcommon', 'libmircommon', '_multibytecodec', '_asyncio',
-        'libpng16', 'libatk', 'libatspi', 'libk5crypto',
-        'libglapi', 'libexpat', 'libgraphite', 'libselinux', 'liblzma',
-        'libxcb', '_csv', 'libz', 'libboost', 'libgcc', '_heapq',
-        'libpangoft2', 'mmap', 'libgpg', 'libXext', 'libwayland', '_random',
-        'libXi', 'termios', '_multiprocessing', '_bisect', 'libgbm', 'grp',
-        'libpangocairo', 'resource', '_queue', 'libXrandr', 'libXrender',
-        'libXcursor', '_posixshmem', 'libthai', '_statistics', '_opcode',
-        'libffi', 'libdatrie', '_contextvars', 'libXxf', 'libXfixes',
-        'libXdmcp', 'libuuid', 'libgmodule', 'libkeyutils', 'libcom',
-        'libxshmfence', 'libgthread', 'clean', 'diff', 'libXau', 'libXdamage',
-        'libXcomposite',
     }
     to_add = [
         {'from': s_dir + '/plugins/libcomposeplatforminputcontextplugin.so',
@@ -49,12 +24,6 @@ libfcitxplatforminputcontextplugin.so'}
     ]
 elif ismacos:
     to_exclude = {
-        'libcrypto', 'libncursesw', 'libssl', 'QtNetwork', 'QtQml', 'QtQuick',
-        'QtSvg', 'QtWebSockets', 'objectify', 'sax', '_bisect',
-        '_bz2', '_codecs', '_ctypes', '_datetime', '_heapq',
-        '_json', '_lzma', '_multibyte', '_opcode', '_pickle',
-        '_random', '_scproxy', '_socket', '_ssl', '_uuid', 'grp',
-        'pyexpat', 'readline', 'resource', 'termios', 'unicodedata',
     }
 
 
